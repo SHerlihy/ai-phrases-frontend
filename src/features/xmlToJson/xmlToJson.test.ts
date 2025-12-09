e8430fce4690b0792be2d45a9abd9a30bc1f4d7c
SHerlihy <steven_herlihy@yahoo.com>
2025-12-09 09:00:14 +0000

    test for list key

--- src/features/xmlToJson/xmlToJson.test.ts
+++ src/features/xmlToJson/xmlToJson.test.ts
@@ -11,6 +11,22 @@ async function getXmlString(xmlFilePath: string): Promise<[undefined, string] | 
     }
 }
 
+const options = {
+    // 1. Strip the XML declaration and attributes like xmlns
+    compact: true,
+    spaces: 4,
+    // 2. Remove the XML namespace prefixes (like xmlns:s3)
+    ignoreAttributes: true,
+    // 3. Force the 'Contents' element into an array even if there is only one
+    // This makes the iteration logic simpler and safer for future data
+    elementNameFn: (name) => {
+        if (name === 'Contents') {
+            return 'ContentsArray';
+        }
+        return name;
+    }
+};
+
 describe("xml to json", ()=>{
     it("returns get response json", async ()=> {
             const [err, xmlString] = await getXmlString('./get.xml');
@@ -19,6 +35,20 @@ describe("xml to json", ()=>{
             }
             
             const getJson = xml2json(xmlString)
-            console.log(getJson)
+    })
+    it("returns list response json", async ()=> {
+            const [err, xmlString] = await getXmlString('./list.xml');
+            if (err) {
+                throw err
+            }
+            
+            const listObj = JSON.parse(xml2json(xmlString, options))
+            let contents = listObj.ListBucketResult.ContentsArray;
+            if (!Array.isArray(contents)){
+                contents = [contents]
+            }
+
+            console.log(contents)
+            expect(contents[0].Key._text === "test")
     })
 })
