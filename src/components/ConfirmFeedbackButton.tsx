8a8d7464439dc84d1a13692c4b9105e145428f2b
SHerlihy <steven_herlihy@yahoo.com>
2025-12-20 16:58:05 +0000

    added confirm feedback tests

---
+++ src/components/ConfirmFeedbackButton.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { FEEDBACK_PENDING } from '@/features/uploadFile/UploadFileModel'
+
+export type Props = {
+    feedback: string,
+    isPending: boolean
+}
+
+const ConfirmFeedbackButton = ({
+    feedback,
+    isPending,
+    ...props
+}: React.ComponentProps<"button"> & Props) => {
+    return (
+        <Button
+            disabled={feedback===FEEDBACK_PENDING}
+        >
+            <p>{feedback}</p>
+        </Button>
+    )
+}
+
+// const ConfirmFeedbackButton = ({
+//     feedback,
+//     isPending,
+//     ...props
+// }: React.ComponentProps<"button"> & Props) => {
+//
+//     const [canConfirm, setCanConfirm] = useState(false)
+//
+//     useEffect(() => {
+//         if (!isPending) {
+//             setCanConfirm(true)
+//         }
+//     }, [isPending])
+//
+//     return (
+//         <Button
+//             variant={canConfirm ? "default" : "ghost"}
+//             className={`
+//                 ${isPending && "bg-yellow-300"}
+//                 ${canConfirm && "bg-lime-300"}
+//             `}
+//             onClick={() => { setCanConfirm(false) }}
+//             disabled={isPending}
+//             {...props}
+//         >
+//             <p>{feedback}</p>
+//         </Button>
+//     )
+// }
+
+export default ConfirmFeedbackButton
