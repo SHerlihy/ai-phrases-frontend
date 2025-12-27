ef7dbd9a5fe457895a510789b3f6c99e3a9dd30d
SHerlihy <steven_herlihy@yahoo.com>
2025-12-27 15:49:05 +0000

    add mock implementation for dev

--- src/features/queryStory/QueryStoryControl.tsx
+++ src/features/queryStory/QueryStoryControl.tsx
@@ -22,14 +22,10 @@ class QueryStoryControl implements IQueryStoryControl {
         const params = new URLSearchParams();
         params.append("key", param)
 
-        const response = await fetch(`${this.postUrl}?${params}`, {
-            method: "POST",
-            mode: "cors",
-            signal: this.controller.signal,
-            body: story
-        })
 
-        if (!response.ok) {
+        const response = await this.markStoryRequest(story, params)
+
+        if (response.status !== 200) {
             return [new Error(`Query story status: ${response.status}`)]
         }
 
@@ -42,6 +38,52 @@ class QueryStoryControl implements IQueryStoryControl {
 
     abortQuery(reason?: any) {
         this.controller.abort(reason)
+    }
+
+    markStoryRequest = async (story: string, params: URLSearchParams) => {
+        if (import.meta.env.DEV) {
+            return await this.markStoryRequestDev()
+        }
+
+        return await this.markStoryRequestProd(story, params)
+    }
+
+
+    markStoryRequestProd = async (story: string, params: URLSearchParams) => {
+
+        return await fetch(`${this.postUrl}?${params}`, {
+            method: "POST",
+            mode: "cors",
+            signal: this.controller.signal,
+            body: story
+        })
+    }
+
+    markStoryRequestDev = async (): Promise<Response> => {
+
+        await new Promise(r => setTimeout(r, 2000));
+
+        const failOpts = {
+            status: 400,
+            statusText: "Error"
+        }
+
+        const successOpts = {
+            status: 200,
+            satusText: "Upload complete"
+        }
+
+        const failResponse = new Response("", failOpts)
+
+        const successResponse = new Response("marked story", successOpts)
+
+        const rnd = Math.random()
+
+        if (rnd > 0.5) {
+            return failResponse
+        }
+
+        return successResponse
     }
 }
 
